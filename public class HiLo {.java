82e3e3d3b12d821e60f2cc42340ab983c5b36c2b
Mlondi <68794263+Mo-Shoba@users.noreply.github.com>
2024-02-21 20:50:37 +0200

    Update public class HiLo {.java

--- public class HiLo {.java
+++ public class HiLo {.java
@@ -6,6 +6,8 @@ public class HiLo {
         // Create a random number for the user to guess
         int theNumber = (int) (Math.random() * 100 + 1);
         System.out.println(theNumber);
+        int guess = 0;
+        System.out.println("Guess a number between 1 and 100:");
     }
 
 }
